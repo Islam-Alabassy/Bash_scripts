69df8a8b00acd7aad8d49593eeae5c71fe2a8488
Islam Alabassy <islamalabassy@gmail.com>
2023-12-19 00:42:24 +0200

    Input as command argument

--- print_file.sh
+++ print_file.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-echo -e "Enter the file to be printed :\n"
-read input_file
+#echo -e "Enter the file to be printed :\n"
+#read input_file
 while read line
 do
 echo $line
-done < $input_file
+done < $1 #input_file
